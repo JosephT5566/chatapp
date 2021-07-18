10d5ca3bc474f0a1a826ab38c562d3a3d3324255
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-07-19 01:48:15 +0800

    add dialog, and component `Contacts` and `Conversations`

--- src/views/payload/sidebar.tsx
+++ src/views/payload/sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
 
 import Tabs, { TabPanel } from '../../components/shared/Tabs';
 import Conversations from './conversations';
@@ -29,7 +30,16 @@ export default function Sidebar(props: { id: string }) {
 	const { id } = props;
 	const classes = useStyle();
 	const [activeKey, setActiveKey] = useState(CONVERSATION_KEY);
+	const [openDialog, setOpenDialog] = useState(false);
 	const isConversation = activeKey === CONVERSATION_KEY;
+
+	const handleOpenDialog = () => {
+		setOpenDialog(true);
+	};
+
+	const handleCloseDialog = () => {
+		setOpenDialog(false);
+	};
 
 	return (
 		<div className={classes.sidebar}>
@@ -45,7 +55,10 @@ export default function Sidebar(props: { id: string }) {
 			<div className={classes.myId}>
 				Your Id: <span>{id}</span>
 			</div>
-			<Button>New {isConversation ? 'Conversation' : 'Contact'}</Button>
+			<Button onClick={handleOpenDialog}>New {isConversation ? 'Conversation' : 'Contact'}</Button>
+			<Dialog open={openDialog} onClose={handleCloseDialog}>
+				<div>123</div>
+			</Dialog>
 		</div>
 	);
 }
