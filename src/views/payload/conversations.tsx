c7d53c5540a44be2d394e369b7ed8b6d1becc54c
Your JosephTseng <zxp930110@hotmail.com.tw>
2021-07-20 15:38:14 +0800

    display conversation list

--- src/views/payload/conversations.tsx
+++ src/views/payload/conversations.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { useConversations } from '../../providers/ConversationsProvider';
 
 export default function Conversations() {
-    return (
-        <div>
-            Conversations
-        </div>
-    )
+	const { conversations } = useConversations();
+
+	return (
+		<div>
+			<List component="nav" aria-label="secondary conversations folders">
+				{conversations.map((conversation, index) => (
+					<ListItem button key={index}>
+						<ListItemText primary={conversation.recipients.map((r) => r.name).join(', ')} />
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
 }
